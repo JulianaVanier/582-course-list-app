892b6fa74b946eba588a9a2fa8a479e6886db64a
Juliana <2295471@edu.vaniercollege.qc.ca>
2023-08-02 16:52:00 -0400

    Firs step - list courses

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,68 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  <p>{{ count }}</p>
+  <p>
+    <CourseItem
+      v-for="course in courses"
+      :course="course"
+      :key="course.id"
+    ></CourseItem>
+  </p>
+  <button @click="addOne">Add to parent</button>
+  <!-- <CourseItem @count-to-parent="addOne" /> -->
+  <!-- <CourseItem v-for="course in courses" :key="course"></CourseItem> -->
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import CourseItem from "./components/CourseItem.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "App",
+  data() {
+    return {
+      count: 0,
+      courses: [
+        {
+          id: 1,
+          name: "Web Adv Tech",
+          description: "Summer course",
+          hours: "25",
+          student: "",
+        },
+        {
+          id: 2,
+          name: "Framework",
+          description: "Winter course",
+          hours: "20",
+          student: "",
+        },
+        {
+          id: 3,
+          name: "Wordpress",
+          description: "Fall course",
+          hours: "16",
+          student: "",
+        },
+        {
+          id: 4,
+          name: "React",
+          description: "Summer course",
+          hours: "54",
+          student: "",
+        },
+      ],
+    };
+  },
   components: {
-    HelloWorld,
+    // HelloWorld,
+    CourseItem,
+  },
+  methods: {
+    addOne() {
+      this.count++;
+    },
   },
 };
 </script>
---
+++ src/components/CourseItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <!-- <h2>Course</h2> -->
+  <!-- <p>{{ count }}</p> -->
+
+  <!-- <button @click="innerAdd">Add</button> -->
+  <p>{{ course.id }}</p>
+  <p>{{ course.name }}</p>
+  <p>{{ course.description }}</p>
+  <p>{{ course.hours }}</p>
+  <!-- <button @click="innerAdd">Add</button> -->
+  <!-- <button @click="outerAdd">Add to parent</button> -->
+</template>
+
+<script>
+export default {
+  name: "CourseItem",
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  props: {
+    course: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    innerAdd() {
+      this.count++;
+    },
+    outerAdd() {
+      this.$emit("count-to-parent");
+    },
+  },
+};
+</script>
